26a580aa077a4427d8b5a3895d07043ac88b93a2
Трофимов Никита Владиславович <Trofimov.Ni.Vl@sberbank.ru>
2020-10-25 18:34:06 +0300

    init project

---
+++ data_types.py
@@ -0,0 +1,39 @@
+#Сегодня мы изучим типы данных
+
+#Строки (strings)
+name = 'Ivan'
+computer = 'Honor'
+best_game = 'brawl stars'
+birth_date = '25.09.2011'
+print(name, computer, best_game, birth_date)
+
+#Числа (numbers)
+age = 9
+year = 2020
+print(age)
+
+#Списки (lists)
+love_games = ['fortnite', 'csgo', 'minecraft']
+numbers = [1, 2, 3, 4, 5, 6, 7]
+print(love_games)
+print(numbers)
+
+#Логические (boolean)
+have_car = False
+is_human = True
+print(have_car)
+print(is_human)
+
+#Кортежи (tuple)
+family = ('brother', 'mother', 'father')
+print(family)
+
+#Множества (sets)
+english_russian = {
+    'red': 'Красный',
+    'yellow': 'Желтый',
+    'apple': 'Яблоко',
+    'car': 'Машина'
+}
+
+print(english_russian['red'])
